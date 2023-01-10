e1240bf3eb4351cf63040511ae68967bd47e6c81
Aleksandr Bedovyi <a.bedovyi@redmadrobot.com>
2023-01-10 17:11:41 +0300

    added popup open when click button_add

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,35 @@
 const initialCards = [
     {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
     },
     {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
     },
     {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
     },
     {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
     },
     {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
     },
     {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
-  ];
-  
+];
+
 const editButton = document.querySelector('.button_edit');
+const addButton = document.querySelector('.button_add');
 const popup = document.querySelector('.popup');
+const popupEdit = document.querySelector('.popup_edit');
+const popupAdd = document.querySelector('.popup_add');
 const closeButton = document.querySelector('.button_close');
 const profileForm = document.querySelector('.popup__form')
 let profileName = document.querySelector('.profile__name');
@@ -34,6 +37,14 @@ let profilePosition = document.querySelector('.profile__position');
 let formElements = document.forms.editProfileForm;
 let popupFieldName = formElements.elements.popupFormTextName;
 let popupFieldPosition = formElements.elements.popupFormTextPosition;
+
+function popupOpenAddForm() {
+    popupAdd.classList.add('popup_opened');
+}
+
+function popupOpenEditForm() {
+    popupEdit.classList.add('popup_opened');
+}
 
 function popupClose() {
     popup.classList.remove('popup_opened');
@@ -43,22 +54,26 @@ function handleFormSubmit(evt) {
     evt.preventDefault();
     let valuePopupFieldName = popupFieldName.value;
     let valuePopupFieldPosition = popupFieldPosition.value;
-
     profileName.textContent = `${valuePopupFieldName}`;
     profilePosition.textContent = `${valuePopupFieldPosition}`;
     popupClose();
 }
+
 profileForm.addEventListener('submit', handleFormSubmit);
 
 editButton.addEventListener('click', (event) => {
     let valueProfileName = profileName.innerHTML;
     let valueProfilePosition = profilePosition.innerHTML;
-    popup.classList.add('popup_opened');
+    popupOpenEditForm();
     popupFieldName.value = `${valueProfileName}`;
     popupFieldPosition.value = `${valueProfilePosition}`;
 });
 
+addButton.addEventListener('click', (event) => {
+    popupOpenAddForm();
+});
 
 closeButton.addEventListener('click', (event) => {
     popupClose();
 });
+
