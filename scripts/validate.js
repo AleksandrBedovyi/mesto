45e39d7d29d1d87699a26c546cc798c331c5c5a4
Aleksandr Bedovyi <a.bedovyi@redmadrobot.com>
2023-02-03 23:52:57 +0300

    added validation of forms

--- scripts/validate.js
+++ scripts/validate.js
@@ -11,50 +11,58 @@ const forms = Array.from(document.querySelectorAll(`${config.form}`));
 const inputs = Array.from(document.querySelectorAll(`${config.input}`));
 const buttons = Array.from(document.querySelectorAll(`${config.submitButton}`));
 
-//function check validity state of input
-const validInput = (input) => {
+const isValid = (form, input) => {
     if (input.validity.valid) {
-        disableInputError(input);
+        hideInputError(form, input);
     } else {
-        enableInputError(input);
+        showInputError(form, input, input.validationMessage);
     }
 }
 
-
-//function add class of error to input
-const enableInputError = (input) => {
-    input.classList.add('popup__field_error');
-}
-
-//function remove class of error from imput
-const disableInputError = (input) => {
-    input.classList.remove('popup__field_error');
-}
-
-//function validate every input in inputs 
-const enableValidate = (inputs) => {
-    inputs.forEach((input) => {
-        input.addEventListener('input', (evt) => {
-            validInput(input);
-        });
+const hasInvalidInput = (inputs) => {
+    return inputs.some((input) => {
+        const result = !input.validity.valid;
+        return result;
     })
 }
 
-enableValidate(inputs);
-
-//function enable submit button
-const enableSubmitButton = (button) => {
-    button.classList.remove('.button_disabled');
+const showInputError = (form, input, errorMessage) => {
+    const errorElement = form.querySelector('.form__input-error');
+    input.classList.add('popup__field_error');
+    errorElement.textContent = errorMessage;
 }
 
-//function disable submit button
-const disableSubmitButton = (button) => {
-    button.classList.add('.button_disabled');
+const hideInputError = (form, input) => {
+    const errorElement = form.querySelector('.form__input-error');
+    input.classList.remove('popup__field_error');
+    errorElement.textContent = '';
 }
 
+const toggleButtonState = (inputs, button) => {
+    if (!hasInvalidInput(inputs)) {
+        button.classList.remove('button_disabled');
+        button.removeAttribute('disabled');
+    } else {
+        button.classList.add('button_disabled');
+        button.setAttribute('disabled', true);
+    }
+}
 
+const setEventListener = (form) => {
+    const button = form.querySelector('.button_save');
+    toggleButtonState(inputs, button);
+    inputs.forEach((input) => {
+        input.addEventListener('input', () => {
+            isValid(form, input);
+            toggleButtonState(inputs, button);
+        });
+    });
+}
 
+const enableValidation = () => {
+    forms.forEach((form) => {
+        setEventListener(form);
+    });
+}
 
-
-
-
+enableValidation();
